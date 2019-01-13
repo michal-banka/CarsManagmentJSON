3d7a15168efbebd60f7554ec2eb1fb29ec541709
michal-banka <banka.michal1964@gmail.com>
2019-01-13 23:40:05 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import models.enums.SortByEnum;
 
 public class Main {
     public static void main(String[] args) {
-        Cars cars = new Cars("D:\\Informatyka korepetycje\\Java\\Mikroservices\\Teoria\\01_PRZYKLADOWY_SYSTEM\\CarCollectionManagment\\src\\main\\resources\\jsonCars.txt");
+        Cars cars = new Cars("jsonCars.txt");
         System.out.println(cars + "\n======================================== COLOR\n");
         cars.sort(SortByEnum.COLOR,false);
         System.out.println(cars + "\n======================================== MODEL\n");
